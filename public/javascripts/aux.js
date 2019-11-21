e3a1ee96da9b2eca63cce3b1f6598e7e490a5ba2
Mike Menendez <mmenendez@csumb.edu>
2019-11-21 11:38:20 -0800

    DB is now setup and initalized, added style elements to modals, added comment snippet how to parse query results

--- public/javascripts/aux.js
+++ public/javascripts/aux.js
@@ -1,5 +1,20 @@
+const conn_url = "mysql://di6bfd3s5livr33s:zlcaatriixgmcr7m@kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k9bje8xxig8ixlpy";
+
+// How to do html in the alert (answers was passed along the chain, we can use a regular var on query results):
+// title: 'All done!',
+// html: `
+//   Your answers:
+//   <pre><code>${answers}</code></pre>
+// `,
+// confirmButtonText: 'Lovely!'
+
 $(".btn").on('click', function() {
-    Swal.queue([{
+    Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        }
+    }).queue([{
             title: "Enter Search Term",
             confirmButtonText: "Next &rarr;",
             input: 'text'
